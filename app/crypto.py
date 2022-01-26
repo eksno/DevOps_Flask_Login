44ca51478a8351396671ee5fd78013b55e97c90e
Jonas. L <jonaslindberg.official@gmail.com>
2022-01-26 11:18:14 +0100

    .gitignore update, linendings are now LF, crpyto work

--- app/crypto.py
+++ app/crypto.py
@@ -1,3 +1,9 @@
 import base64
-import imp
+import base64
 import hashlib
+from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
+
+class AESCipher:
+    def __init__(self, ) -> None:
+        key = None
